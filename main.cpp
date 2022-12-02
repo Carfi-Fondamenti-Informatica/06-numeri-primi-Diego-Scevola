13a5c8c3d6bc79410def433bf7c3f7179f37a008
Diego Scevola <117745592+Diego-Scevola@users.noreply.github.com>
2022-12-03 00:55:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
-int main(){
-  return 0;
+bool primi(int a);
+
+using namespace std;
+
+int main() {
+    int a;
+    bool b;
+
+    cin>>a;
+    b= primi(a);
+
+    if(b== true)
+        cout<<"numero primo"<<endl;
+    else if(b== false)
+        cout<<"numero non primo"<<endl;
+    return 0;
+}
+
+bool primi(int a) {
+    return false;
 }
