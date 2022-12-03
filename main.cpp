87ae867a3554a57195bd51be67798e1e5277c28c
Diego Scevola <117745592+Diego-Scevola@users.noreply.github.com>
2022-12-03 11:13:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include "l1b.h"
 using namespace std;
 
-int main(){
-    int a;
-    bool b;
-    
-    cin>>a;
-    b=primi(a);
-    
-    if(b==true)
-        cout<<"numero primo"<<endl;
-    else if(b==false)
-        cout<<"numero non primo"<<endl;
-    return 0;
-      }
+bool primi (int n) {
+int div=1, conta=0;
+
+    while (conta<=1 && div<=n/2){
+        if (n%div==0){
+            conta++;
+        }
+        div++;
+    }
+    if ((conta==1))
+    {
+        return true;
+    } else{
+        return false;
+    }
+}
